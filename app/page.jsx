24b916baaa6364626b563c85c9a795bb8c4142cb
AlexVOiceover <alex@alexvoiceover.com>
2024-03-19 10:53:09 +0000

    check update speed

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <section className='py-24'>
       <div className='container'>
-        <h1 className='mt-4 text-3xl font-bold'>NextJS Alex</h1>
+        <h1 className='mt-4 text-3xl font-bold'>NextJS Vercel</h1>
       </div>
     </section>
   )
