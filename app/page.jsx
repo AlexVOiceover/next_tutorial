7342e914a22fbfb1d707da5d784779fd79991ad7
AlexVOiceover <alex@alexvoiceover.com>
2024-03-19 10:51:25 +0000

    Deployed on Vercel

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <section className='py-24'>
       <div className='container'>
-        <h1 className='mt-4 text-3xl font-bold'>NextJS</h1>
+        <h1 className='mt-4 text-3xl font-bold'>NextJS Alex</h1>
       </div>
     </section>
   )
