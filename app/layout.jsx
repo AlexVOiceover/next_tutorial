cb2eeb96d8d405f42d2ad60a3685a18761c452ec
Alexander <alex@alexvoiceover.com>
2024-04-01 23:40:58 +0100

    transfer

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,8 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
+import Header from '@/components/layout/header'
+import Footer from '@/components/layout/footer'
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        <main> {children}</main>
+        <Footer />
+      </body>
     </html>
   )
 }
--- killer.sh
+++ killer.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 # chmod +x killer.sh
 
+# Default to port 3000 if no argument is given, otherwise use the provided argument
+PORT=${1:-3000}
+
 # Find the process using port 3000
-PID=$(lsof -t -i:3000)
+PID=$(lsof -t -i:$PORT)
 
 # Check if the PID variable is set to a non-empty string
 if [ -n "$PID" ]; then
-  echo "Killing process on port 3000 with PID: $PID"
+  echo "Killing process on port $PORT with PID: $PID"
   kill -9 $PID
 else
-  echo "No process found on port 3000."
+  echo "No process found on port $PORT."
 fi
